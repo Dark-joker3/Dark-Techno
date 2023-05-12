ee5199d392efc2ecc5ae2cb7a8e588f04fd2ef3c
Black-KDX <121096454+Dark-joker3@users.noreply.github.com>
2023-05-12 15:52:41 +0530

    Add files via upload

---
+++ plugins/owner-tagall.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(`${text ? `${text}\n` : ''}┌─「 🇵🇰Tag All🇵🇰 」\n` + users.map(v => '│🇵🇰 @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+        mentions: users
+    })
+}
+
+handler.help = ['o-tagall']
+handler.tags = ['owner']
+handler.command = ['o-tagall']
+handler.owner = true
+handler.group = true
+
+export default handler
